5a583207e26db8269299ee875003dc6ca937076f
Max <maxim.valenko@farfetch.com>
2021-02-01 11:06:31 +0000

    CI

--- webpack.config.js
+++ webpack.config.js
@@ -3,75 +3,98 @@
 const path = require("path");
 const webpack = require("webpack");
 const { ConcatSource } = require("webpack-sources");
+const ZipPlugin = require("zip-webpack-plugin");
 
 const packageJson = require("./package.json");
 
-module.exports = {
-  entry: "./src/index.tsx",
-  mode: "production",
-  devtool: false,
-  optimization: {
-    minimize: false,
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.m?jsx?$/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              [
-                "@babel/preset-env",
-                {
-                  targets: {
-                    ie: "8",
+module.exports = ({ zip }) => {
+  return {
+    entry: "./src/index.tsx",
+    mode: "production",
+    devtool: false,
+    optimization: {
+      minimize: false,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.m?jsx?$/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                [
+                  "@babel/preset-env",
+                  {
+                    targets: {
+                      ie: "8",
+                    },
+                    useBuiltIns: "entry",
+                    corejs: "3",
                   },
-                  useBuiltIns: "entry",
-                  corejs: "3",
-                },
+                ],
               ],
-            ],
+            },
           },
         },
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  output: {
-    filename: "script.js",
-    path: path.resolve(__dirname, "dist"),
-  },
-  plugins: [
-    new (class {
-      apply(compiler) {
-        compiler.hooks.emit.tapAsync("FileListPlugin", (
-          /**@type webpack.Compilation */ compilation,
-          callback
-        ) => {
-          const asset = compilation.assets["script.js"];
-          if (asset) {
-            const bundleSource = asset.source();
-            const { name, version, pixinsight: { script } = {} } = packageJson;
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+      filename: "script.js",
+      path: path.resolve(__dirname, "dist"),
+    },
+    plugins: [
+      new (class {
+        apply(compiler) {
+          compiler.hooks.thisCompilation.tap(
+            "FileListPlugin",
+            (compilation) => {
+              compilation.hooks.processAssets.tapPromise(
+                {
+                  name: "FileListPlugin",
+                  stage:
+                    webpack.Compilation.PROCESS_ASSETS_STAGE_OPTIMIZE_TRANSFER,
+                },
+                () => {
+                  const asset = compilation.assets["script.js"];
+                  if (asset) {
+                    const bundleSource = asset.source();
+                    const {
+                      name,
+                      version,
+                      pixinsight: { script } = {},
+                    } = packageJson;
 
-            const newSource = [
-              `#feature-id ${script["feature-id"] || name}`,
-              `#feature-info ${script["feature-info"] || name}`,
-              bundleSource,
-            ].join("\n");
+                    const newSource = [
+                      `#feature-id ${script["feature-id"] || name}`,
+                      `#feature-info ${script["feature-info"] || name}`,
+                      bundleSource,
+                    ].join("\n");
 
-            compilation.assets["script.js"] = new ConcatSource(newSource);
-          }
-          callback();
-        });
-      }
-    })(),
-  ],
+                    compilation.assets["script.js"] = new ConcatSource(
+                      newSource
+                    );
+                  }
+                  return Promise.resolve();
+                }
+              );
+            }
+          );
+        }
+      })(),
+      zip &&
+        new ZipPlugin({
+          filename: "StarDe-emphasizer.zip",
+          pathPrefix: "src/scripts/AstroSwell/StarDe-emphasizer",
+        }),
+    ].filter(Boolean),
+  };
 };
